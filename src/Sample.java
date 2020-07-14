d1d84212f0c671d4245e525d61ef0e3f70c069ae
vinod kumar <vk1999devil@gmail.com>
2020-07-14 07:08:58 +0530

    initial  code for github

---
+++ src/Sample.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Sample {
+    public static void main(String[] args) {
+        JFrame myWindow;
+        myWindow =new JFrame();
+        myWindow.setTitle("frame is here");
+        myWindow.setSize(1600,900);
+        myWindow.setVisible(true);
+    }
+}
